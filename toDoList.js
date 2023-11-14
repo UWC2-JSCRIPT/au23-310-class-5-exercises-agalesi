7ebe34daf9c01e0db47c0a3dc0a8f4d72869b492
agalesi <81053127+agalesi@users.noreply.github.com>
2023-11-14 21:58:06 +0100

    Update toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,45 @@
 // If an li element is clicked, toggle the class "done" on the <li>
 
+
+let list = document.querySelectorAll('li'); //in case other list elements are added
+let itemsList = list.length;
+for (let i = 0; i < itemsList; i++) {
+  list[i].addEventListener('click', (e) => {
+    toggleDone(e);
+  });
+}
+
+function toggleDone(e) {
+  e.currentTarget.className = 'done';
+}
+
 // If a delete link is clicked, delete the li element / remove from the DOM
+
+const deleteLink = document.getElementsByClassName('delete');
+
+for (const link of deleteLink) {
+link.addEventListener('click', function onClick() { 
+
+  link.parentElement.remove();
+ 
+}); }
+
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
+
 const addListItem = function(e) {
   e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
+  const input = this.parentNode.getElementsByTagName('input')[0]; // get current input by user
   const text = input.value; // use this text to create a new <li>
+  //const uList = document.getElementsByClassName('today-list');   
+  const listElement = document.createElement('li'); //create a new list element and the node
+  const node = document.createTextNode(text);  
+  listElement.appendChild(node);
+  uList.appendChild(listElement);}
 
-  // Finish function here
-};
+ 
+ const uList = document.getElementsByClassName('today-list')[0];  
+ uList.addEventListener('click', (e) => {if (e.target.closest('add-item')) {addListItem(e)}});
+
