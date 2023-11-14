a60ec590f858217a14edd1b36afbb38031d35e2d
agalesi <81053127+agalesi@users.noreply.github.com>
2023-11-14 21:29:36 +0100

    Update stoppingBehavior.js

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -6,5 +6,22 @@ document.getElementById('cat').addEventListener('click', () => {
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+let change = document.getElementById('more-info');
+
+change.addEventListener('click', function(e) {
+ if (e.target.id === change.id) {
+   e.preventDefault();
+  alert('Here\'s some info');
+}});
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+let change2 = document.getElementById('dog');
+
+change2.addEventListener('click', function(e) {
+ if (e.target.id === change2.id) {
+   e.preventDefault();
+  alert('Bow wow');
+  e.stopPropagation();
+}});
